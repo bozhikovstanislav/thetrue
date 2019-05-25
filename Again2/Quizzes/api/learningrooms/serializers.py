28eee7d745443fce7444749f46cdb1b9796d9e2f
stanislav bozhikov <bozhikov.stanislav@gmail.com>
2019-05-25 14:57:11 +0300

    Ужаст Малко Айдееее

--- Again2/Quizzes/api/learningrooms/serializers.py
+++ Again2/Quizzes/api/learningrooms/serializers.py
@@ -1,7 +1,8 @@
-from learningrooms.models import RoomQuizze
 from rest_framework import serializers
+
+from learningrooms.models import RoomQuizze
 
 
 class RoomQuizzesSerilizer(serializers.ModelSerializer):
     model = RoomQuizze
-    fields = ('id','name','type_on_room','category','profession')
+    fields = ('id', 'name', 'type_on_room', 'category', 'profession')
